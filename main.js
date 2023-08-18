34ba35dacf86bd586210f323c77c0099a70ac76f
Kate <violetds0826@gmail.com>
2023-08-18 14:34:48 -0400

    changed toggle btn javascript

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
 /* Toggle Button*/
 const navbarMenu = document.querySelector('.header-menu')
 const toggleBtn = document.querySelector('.navbar-toggleBtn')
-const overlay = document.querySelector('.main-background-overlay');
 
 toggleBtn.addEventListener('click', () => {
-    let toggle = navbarMenu.classList.toggle('open')
-    if (toggle) {
-        overlay.className += " responsive";
-    } else {
-        overlay.className += "main-background-overlay";
-    }
+    navbarMenu.classList.toggle('open')
 })
 //Navbar menu click 
 navbarMenu.addEventListener('click', () => {
